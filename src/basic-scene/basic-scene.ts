d69b9b010876ee82c4005b77453ff1119d014b36
adrien gerard <adrien.gerard@envestboard.com>
2022-11-21 23:59:54 +0100

    add basic scene

--- src/basic-scene/basic-scene.ts
+++ src/basic-scene/basic-scene.ts
@@ -1,6 +1,38 @@
-import * as three from "three";
+import { BoxGeometry, Mesh, MeshBasicMaterial, PerspectiveCamera, Scene, WebGLRenderer } from 'three';
 
-const scene = new three.Scene();
+type ScreenSize = { width: number; height: number };
+export class BasicScene {
+  screenSize: ScreenSize;
+  fov: number;
+  constructor(
+    screenSize: ScreenSize = { width: 800, height: 600 },
+    fov: number = 75
+  ) {
+    this.screenSize = screenSize;
+    this.fov = fov;
+  }
 
-const cube = new three.BoxGeometry(1, 1, 1);
-const material = new three.MeshBasicMaterial({ color: "red" });
+  render() {
+    const canvas = document.querySelector("#webgl") as HTMLCanvasElement;
+    const scene = new Scene();
+
+    const cube = new BoxGeometry(1, 1, 1);
+    const material = new MeshBasicMaterial({ color: 0xff0000 });
+    const mesh = new Mesh(cube, material);
+
+    const camera = new PerspectiveCamera(
+      this.fov,
+      this.screenSize.width / this.screenSize.height
+    );
+    camera.position.setZ(4);
+    camera.position.setX(2);
+    scene.add(mesh);
+    scene.add(camera);
+
+    const renderer = new WebGLRenderer({
+      canvas,
+    });
+    renderer.setSize(this.screenSize.width, this.screenSize.height);
+    renderer.render(scene, camera);
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,12 @@
-import "./style.css";
+import './style.css';
+
+import { BasicScene } from './basic-scene/basic-scene';
+
+export class Application {
+  main() {
+    const scene = new BasicScene();
+    scene.render();
+  }
+}
+
+new Application().main();
